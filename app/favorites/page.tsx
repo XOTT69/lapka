4b2787198b9f140415e7f6ad2e85dc9bcf121d53
Антон <mikolenko.anton1@gmail.com>
2026-09-24 12:51:05 +0300

    Add working favorites page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,5 @@
+import FavoritesView from '@/components/favorites-view';
+
+export default function FavoritesPage(){
+ return <main className="wrap page favoritesPage"><div className="simplePageHead"><span>Збережене</span><h1>Обране</h1></div><FavoritesView/></main>
+}
