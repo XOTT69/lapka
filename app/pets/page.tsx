968b2bcaabb77e332e823c0fb33eafd6a4f957e1
Антон <mikolenko.anton1@gmail.com>
2026-09-24 03:05:02 +0300

    Support multiple pets and hide supplier identity

--- app/pets/page.tsx
+++ app/pets/page.tsx
@@ -1,2 +1,1 @@
-import { redirect } from 'next/navigation';
-export default function Pets(){ redirect('/account#pet') }
+import {redirect} from 'next/navigation';export default function Pets(){redirect('/account#pets')}
